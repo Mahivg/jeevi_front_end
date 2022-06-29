2789343b5ee51d2f62b38c6a525b37eea96acc62
MageshVg5 <vgmagesh5@gmail.com>
2022-06-29 08:49:40 +0530

    Added till Array methods and conditional statements

---
+++ JS/my_arrays.js
@@ -0,0 +1,112 @@
+var numArray = [1,2,3,4,5,6,7,8,9,10];
+
+console.log(numArray.length);
+
+// default methods supported in array object
+
+// Adding an element
+
+console.log(numArray);
+
+numArray.push(11);
+
+console.log(numArray);
+
+//read 0 to array.length -1
+console.log(numArray[1]);
+
+// remove an element
+
+numArray.pop();
+// it will remove element in (numArray.length -1)
+
+console.log(numArray); // abc
+
+console.log(numArray.slice(1, 3)); // xyz
+
+
+console.log(numArray.splice(numArray.length - 1, 1))
+console.log(numArray);
+
+// Iterating
+// loop
+
+// for(let i = 0; i < numArray.length - 1; i = i+2) {
+//     console.log(numArray[i]);
+// }
+
+// for(let num in numArray) {
+//     console.log(num);
+// }
+
+
+// it should be iterated completely
+// numArray.forEach(function(num){
+//     console.log(num);
+// });
+
+var i = 0;
+
+// while(i < numArray.length) {
+//     console.log(numArray[i]);
+//     i++;
+// }
+
+do {
+    console.log(numArray[i]);
+    i++;
+} while(i < numArray.length);
+
+
+// Finding elements
+
+const numFind = numArray.find(function(num) {
+    return num == 1;
+});
+// null or undefined
+console.log(numFind);
+
+// finding index of an element
+
+
+const elemIndex = numArray.findIndex(function(num){
+    return num == 11;
+});
+// return index if condition becomes true or else it will return -1
+if(elemIndex == -1) {
+    console.log("Element not found in that array");
+} else {
+    console.log(elemIndex);
+}
+
+// Array Modification, current array will be changed to New array
+
+// i want to find even numbers
+    
+var evenArray = numArray.filter(function(num){
+    return (num % 2 == 0);
+});
+
+console.log(evenArray);
+console.log(numArray);
+
+
+// Map, convert array to new a new form
+//[1,2,3,4,5,6,7,8,9,10]
+var mappedArray = numArray.map(function(num) {
+    if(num % 2 == 0) {
+        return num + "_even";
+    } else {
+        return num + "_odd";
+    }
+});
+
+console.log(mappedArray);
+
+
+
+
+
+
+
+
---
+++ JS/my_op.js
@@ -0,0 +1,10 @@
+// ES5
+// const Employee = require('./object_es6.js');
+
+import Employee from "./object_es6.js";
+
+
+var emp1 = new Employee("stud01", "Jeevi", 2016 );
+var emp2 = new Employee("stud02", "Arasu", 2011);
+
+console.log(emp1);
